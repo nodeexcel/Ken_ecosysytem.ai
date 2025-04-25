da8a5ab710f3ca11cd7e8e3757e040acde77ac0f
Atul <atul.etech2011@gmail.com>
2025-04-25 16:20:50 +0530

    minor issue

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar'
 function Dashboard({ children }) {
     return (
         <div className='w-full flex h-screen space-y-6 overflow-y-auto'>
-            <div className='w-[5%]'>
+            <div className='w-[5%] min-w-[53px]'>
             <Sidebar />
             </div>
             <div className='w-[95%] bg-[#F6F7F9]'>
