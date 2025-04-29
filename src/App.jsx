48c7a110b004f3accc4764be17bb5069224b25a7
Atul <atul.etech2011@gmail.com>
2025-04-29 08:17:13 +0200

    fixes the issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ import Dashboard from './layout/Dashboard'
 import SetPassword from './pages/createPassword'
 import Success from './pages/success'
 import PaymentFailed from './pages/failure'
-import Pricing from './pages/pricing'
 import ResetPassword from './pages/ResetPassword'
+import PricingPage from './pages/Pricing'
 
 
 function App() {
 
   return (
     <div className='h-screen'>
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/create-password' element={<SetPassword />} />
-      <Route path='/reset-password' element={<ResetPassword />} />
-      <Route path='/dashboard' element={<Dashboard><SettingsPage /></Dashboard>} />
-      <Route path='/pricing' element={<Pricing />} />
-      <Route path='/success' element={<Success />} />
-      <Route path='/cancel' element={<PaymentFailed />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/create-password' element={<SetPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/dashboard' element={<Dashboard><SettingsPage /></Dashboard>} />
+        <Route path='/pricing' element={<PricingPage />} />
+        <Route path='/success' element={<Success />} />
+        <Route path='/cancel' element={<PaymentFailed />} />
+      </Routes>
     </div>
   )
 }
