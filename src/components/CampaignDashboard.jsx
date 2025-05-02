3556f4eb19cb98e2e3a532580b4e4694a559ba02
Atul <atul.etech2011@gmail.com>
2025-05-02 16:31:26 +0530

    fix the issue

--- src/components/CampaignDashboard.jsx
+++ src/components/CampaignDashboard.jsx
@@ -32,7 +32,7 @@ function CampaignDashboard() {
     };
 
     return (
-        <div className="w-full p-4 flex flex-col gap-4 font-sans onest">
+        <div className="w-full p-4 flex flex-col gap-4 onest ">
             <div className="flex justify-between items-center">
                 <h1 className="text-gray-900 font-semibold text-xl md:text-2xl">Campaigns</h1>
                 <button className="bg-[#675FFF] text-white rounded-md text-sm md:text-base px-4 py-2">
@@ -43,13 +43,13 @@ function CampaignDashboard() {
             <div className="overflow-auto">
                 <table className="min-w-full rounded-2xl">
                     <thead>
-                        <tr className="text-left text-[16px] font-[400] text-[#5A687C]">
-                            <th className="px-6 py-3">Name</th>
-                            <th className="px-6 py-3">Sent (date)</th>
-                            <th className="px-6 py-3">Sent to (numbers)</th>
-                            <th className="px-6 py-3">Status</th>
-                            <th className="px-6 py-3">Lead Status</th>
-                            <th className="px-6 py-3">Actions</th>
+                        <tr className="text-left text-[#5A687C]">
+                            <th className="px-6 py-3 text-[16px] font-medium">Name</th>
+                            <th className="px-6 py-3 text-[16px] font-medium">Sent (date)</th>
+                            <th className="px-6 py-3 text-[16px] font-medium">Sent to (numbers)</th>
+                            <th className="px-6 py-3 text-[16px] font-medium">Status</th>
+                            <th className="px-6 py-3 text-[16px] font-medium">Lead Status</th>
+                            <th className="px-6 py-3 text-[16px] font-medium">Actions</th>
                         </tr>
                     </thead>
                     <tbody className='bg-white shadow-sm'>
@@ -64,7 +64,7 @@ function CampaignDashboard() {
                                     <p className="bg-green-100 text-green-700 px-2 py-1 text-xs rounded-full">
                                         {item.status ? 'Active' : 'Paused'}
                                     </p>
-                                    <p className="relative inline-flex items-center cursor-pointer">
+                                    <label className="relative inline-flex items-center cursor-pointer">
                                         <input
                                             type="checkbox"
                                             className="sr-only peer"
@@ -72,7 +72,7 @@ function CampaignDashboard() {
                                             onChange={() => toggleStatus(index, 'status')}
                                         />
                                         <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-indigo-400 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-indigo-600"></div>
-                                    </p>
+                                    </label>
                                     </div>
                                 </td>
 
@@ -81,7 +81,7 @@ function CampaignDashboard() {
                                    <p className="bg-green-100 text-green-700 px-2 py-1 text-xs rounded-full">
                                         {item.leadStatus ? 'Active' : 'Inactive'}
                                     </p>
-                                    <p className="relative inline-flex items-center cursor-pointer">
+                                    <label className="relative inline-flex items-center cursor-pointer">
                                         <input
                                             type="checkbox"
                                             className="sr-only peer"
@@ -89,7 +89,7 @@ function CampaignDashboard() {
                                             onChange={() => toggleStatus(index, 'leadStatus')}
                                         />
                                         <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-indigo-400 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-indigo-600"></div>
-                                    </p>
+                                    </label>
                                    </div>
                                 </td>
 
